82e39fe1234ff5183b5e5fe10ef6927b2379cc56
Gammaglobulino <mazzantia@hotmail.com>
2021-03-11 17:43:47 +0100

    http server bones

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
+struct Server{
+    ip_addr:String,
+}
+
+impl Server{
+    fn new(addr:String)->Server{
+        Server{
+            ip_addr:addr
+        }
+    }
+    fn run(self){
+        println!("running on: {}",self.ip_addr);
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let server=Server::new(String::from("127.0.0.1:8080"));
+    server.run();
 }
