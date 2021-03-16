0eedea9a622b9e4e73ffb5f12cf6e59b0e987257
Gammaglobulino <mazzantia@hotmail.com>
2021-03-16 18:36:36 +0100

    implemented server listener

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,7 @@
 
+    use std::net::TcpListener;
+    use std::io::Read;
+
     pub struct Server {
         ip_addr: String,
     }
@@ -9,5 +12,21 @@
         }
         pub fn run(self) {
             println!("running on: {}", self.ip_addr);
+            let listener=TcpListener::bind(&self.ip_addr).unwrap();
+            loop{
+                match listener.accept(){
+                    Ok((mut stream,_))=>{
+                        let mut buffer=[0;1024];
+                        match stream.read(&mut buffer){
+                            Ok(_)=>{
+                                println!("Data received from request: {}",String::from_utf8_lossy(&buffer));
+                            },
+                            Err(e)=> println!("Failed to receive data from the buffer {}",e),
+                        }
+
+                    },
+                    Err(e) => println!("{}",e),
+                }
+            }
         }
     }
