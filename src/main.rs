c70c92a3997bd7f7f0c952e377e7c168232c1a55
Gammaglobulino <mazzantia@hotmail.com>
2021-05-24 17:05:44 +0200

    working with env_variable & serving static html files

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use server::Server;
 use http::HttpMethods;
 use http::Request;
 use website_handler::Websitehandler;
+use std::env;
 
 mod http;
 mod server;
 mod website_handler;
 
 fn main() {
+    let default_path=format!("{}/public",env!("CARGO_MANIFEST_DIR"));
+    let puplic_path=env::var("PUBLIC PATH").unwrap_or(default_path);
+    println!("The public path is: {}",puplic_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(Websitehandler);
+    server.run(Websitehandler::new(puplic_path));
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,14 +1,27 @@
 use super::server::Handler;
 use super::http::{Request,Response,StatusCode,HttpMethods};
+use std::fs;
 
-pub struct Websitehandler;
+
+pub struct Websitehandler{
+    public_path:String
+}
+impl Websitehandler{
+    pub fn new(public_path:String)->Self{
+        Self{public_path}
+    }
+    pub fn read_file(&self,file_path:&str)->Option<String>{
+        let path=format!("{}/{}",self.public_path,file_path);
+        fs::read_to_string(path).ok()    
+    }
+}
 
 impl Handler for Websitehandler{
     fn handle_request(&mut self,request: &Request)-> Response{
         match request.method(){
             HttpMethods::GET => {
                 match request.path(){
-                    "/" => Response::new(StatusCode::Ok,Some("<h1> You're homie babe</h1>".to_string())),
+                    "/" => Response::new(StatusCode::Ok,self.read_file("index.html")),
                     "/hello" => Response::new(StatusCode::Ok,Some("<h1> Hello mate!</h1>".to_string())),    
                     _ => Response::new(StatusCode::NotFound,None),
                          
